66d4d0687d31fa3704b9862eb1707590afdd79a2
Kilian Koeltzsch <me@kilian.io>
2018-08-12 21:33:16 +0200

    use dirs crate to correctly handle home_dir

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+extern crate dirs;
 extern crate readnow;
 
 use std::env;
 use std::process;
+use std::path;
+
+use dirs::home_dir;
 
 use readnow::Config;
 
 fn main() {
     let plist_path = env::var("SAFARI_BOOKMARKS_PATH").unwrap_or_else(|_| {
-        "~/Library/Safari/Bookmarks.plist".to_owned()
+        let path = path::PathBuf::from("Library/Safari/Bookmarks.plist");
+        let dir = home_dir().unwrap().join(path);
+        dir.to_str().unwrap().to_owned()
     });
     let config = Config::new(plist_path);
 
