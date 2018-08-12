1bfd3940b1dad81af2c0b809455f36998bc137d1
Kilian Koeltzsch <me@kilian.io>
2018-08-13 00:44:04 +0200

    add ReadnowLibTests to LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,9 @@
 import XCTest
 
 import readnowTests
+import ReadnowLibTests
 
 var tests = [XCTestCaseEntry]()
 tests += readnowTests.allTests()
+tests += ReadnowLibTests.allTests()
 XCTMain(tests)
