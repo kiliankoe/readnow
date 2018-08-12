0bbc82bb3d69ecee4f39135992c813a0aca5b20f
Kilian Koeltzsch <me@kilian.io>
2018-08-13 01:02:31 +0200

    fix formula sha checksum

--- Formula/readnow.rb
+++ Formula/readnow.rb
@@ -2,7 +2,7 @@ class Readnow < Formula
     desc "Open a random link from your Safari reading list"
     homepage "https://github.com/kiliankoe/readnow"
     url "https://github.com/kiliankoe/readnow/archive/0.1.0.tar.gz"
-    sha256 "5490e9ca561af0f03e3727b8022d2e2947927989d6ac7c0cbee2b3cf59022cd2"
+    sha256 "3928f352e5ec316e3f5ebc3cf493498b2c08a6014740d5186b5466ae5e4ff2a2"
     head "https://github.com/kiliankoe/readnow.git"
 
     depends_on :xcode
