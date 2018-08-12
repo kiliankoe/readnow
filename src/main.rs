7163e9a1299d041c15efe556f4a3d9496fe89812
Kilian Koeltzsch <me@kilian.io>
2018-08-12 20:00:24 +0200

    very hacky first implementation, works 👌

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+extern crate readnow;
 
+use std::env;
+use std::process;
+
+use readnow::Config;
+
+fn main() {
+    let plist_path = env::var("SAFARI_BOOKMARKS_PATH").unwrap_or_else(|_| {
+        "~/Library/Safari/Bookmarks.plist".to_owned()
+    });
+    let config = Config::new(plist_path);
+
+    if let Err(e) = readnow::run(config) {
+        eprintln!("Encountered error: {}", e);
+        process::exit(1);
+    }
 }
