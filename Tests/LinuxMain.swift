2449c7e7aaad1d85f10dc0f127fe2cff441f47eb
Kilian Koeltzsch <me@kilian.io>
2018-08-13 00:45:02 +0200

    remove linux test support
    not like this runs on ¬macOS anyways 🤷‍♀️

--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-import readnowTests
-import ReadnowLibTests
-
-var tests = [XCTestCaseEntry]()
-tests += readnowTests.allTests()
-tests += ReadnowLibTests.allTests()
-XCTMain(tests)
