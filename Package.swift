d7c1e3d5adc0b9305e812915878579471687f9f0
Saeed Taheri <saeed.taheri@gmail.com>
2020-07-28 11:10:07 +0430

    Print the url to be opened to the console.

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "readnow",
-		platforms: [.macOS(.v10_12)],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "readnow",
-            dependencies: ["ReadnowLib"]),
-        .target(
-            name: "ReadnowLib",
-            dependencies: []),
-        .testTarget(
-            name: "readnowTests",
-            dependencies: ["readnow"]),
-        .testTarget(
-            name: "ReadnowLibTests",
-            dependencies: ["ReadnowLib"]),
-    ]
+	name: "readnow",
+	platforms: [.macOS(.v10_12)],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(
+			name: "readnow",
+			dependencies: ["ReadnowLib"]),
+		.target(
+			name: "ReadnowLib",
+			dependencies: []),
+		.testTarget(
+			name: "readnowTests",
+			dependencies: ["readnow"]),
+		.testTarget(
+			name: "ReadnowLibTests",
+			dependencies: ["ReadnowLib"]),
+	]
 )
--- Sources/ReadnowLib/Readnow.swift
+++ Sources/ReadnowLib/Readnow.swift
@@ -1,60 +1,62 @@
 import Cocoa
 
 struct Bookmarks: Decodable {
-    let Children: [Child]
+	let Children: [Child]
 
-    struct Child: Decodable {
-        let Title: String?
-        let Children: [Item]?
+	struct Child: Decodable {
+		let Title: String?
+		let Children: [Item]?
 
-        struct Item: Decodable {
-            let URLString: String?
-        }
-    }
+		struct Item: Decodable {
+			let URLString: String?
+		}
+	}
 
-    var readingList: Child? {
-        return self.Children.first { child in
-            child.Title == "com.apple.ReadingList"
-        }
-    }
+	var readingList: Child? {
+		return self.Children.first { child in
+			child.Title == "com.apple.ReadingList"
+		}
+	}
 
-    init(fromPath path: URL) throws {
-        let data = try Data(contentsOf: path)
-        self = try PropertyListDecoder().decode(Bookmarks.self, from: data)
-    }
+	init(fromPath path: URL) throws {
+		let data = try Data(contentsOf: path)
+		self = try PropertyListDecoder().decode(Bookmarks.self, from: data)
+	}
 }
 
 func randomReadingListElement() throws -> Bookmarks.Child.Item {
-    guard #available(macOS 10.12, *) else {
-        throw "this tool requires macOS >=10.12"
-    }
+	guard #available(macOS 10.12, *) else {
+		throw "this tool requires macOS >=10.12"
+	}
 
-    let plistPath = ProcessInfo.processInfo.environment["SAFARI_BOOKMARKS_PATH"]
-        ?? "\(FileManager.default.homeDirectoryForCurrentUser)Library/Safari/Bookmarks.plist"
+	let plistPath = ProcessInfo.processInfo.environment["SAFARI_BOOKMARKS_PATH"]
+		?? "\(FileManager.default.homeDirectoryForCurrentUser)Library/Safari/Bookmarks.plist"
 
-    guard let plistURL = URL(string: plistPath) else {
-        throw "\(plistPath) is not a valid file URL"
-    }
+	guard let plistURL = URL(string: plistPath) else {
+		throw "\(plistPath) is not a valid file URL"
+	}
 
-    let bookmarks = try Bookmarks(fromPath: plistURL)
+	let bookmarks = try Bookmarks(fromPath: plistURL)
 
-    guard let readingList = bookmarks.readingList else {
-        throw "no reading list found"
-    }
+	guard let readingList = bookmarks.readingList else {
+		throw "no reading list found"
+	}
 
-    guard let randomElement = readingList.Children?.randomElement() else {
-        throw "no elements found in reading list"
-    }
+	guard let randomElement = readingList.Children?.randomElement() else {
+		throw "no elements found in reading list"
+	}
 
-    return randomElement
+	return randomElement
 }
 
 public func run() throws {
-    let randomElement = try randomReadingListElement()
+	let randomElement = try randomReadingListElement()
 
 	guard let urlStr = randomElement.URLString, let url = URL(string: urlStr) else {
-        throw "\(randomElement.URLString ?? "---") is not a valid URL"
-    }
+		throw "\(randomElement.URLString ?? "---") is not a valid URL"
+	}
 
-    NSWorkspace.shared.open(url)
+	print(url)
+
+	NSWorkspace.shared.open(url)
 }
